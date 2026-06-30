4e663c61223178f9dc9f1a5b26ad3c3a00d6e6a2
krnlllllllllllllll <jworley419@gmail.com>
2026-06-29 20:02:52 -0400

    Refactor MyButtonSettingV3 and add PlaytestMenu

--- src/MyButtonSettingV3.cpp
+++ src/MyButtonSettingV3.cpp
@@ -1,89 +1,15 @@
-#include <Geode/loader/SettingV3.hpp>
-#include <Geode/loader/Mod.hpp>
-#include <Geode/binding/ButtonSprite.hpp>
-#include <Geode/binding/FLAlertLayer.hpp>
+#include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
+#include <Geode/ui/ScrollLayer.hpp>
+#include <Geode/ui/GeodeUI.hpp>
 
 using namespace geode::prelude;
 
-class MyButtonSettingV3 : public SettingV3 {
+class PlaytestMenu : public Popup {
 public:
-    static Result<std::shared_ptr<SettingV3>> parse(std::string const& key, std::string const& modID, matjson::Value const& json) {
-        auto res = std::make_shared<MyButtonSettingV3>();
-        auto root = checkJson(json, "MyButtonSettingV3");
-        res->init(key, modID, root);
-        res->parseNameAndDescription(root);
-        res->parseEnableIf(root);
-        root.checkUnknownKeys();
-        return root.ok(std::static_pointer_cast<SettingV3>(res));
-    }
-
-    bool load(matjson::Value const& json) override {
-        return true;
-    }
-    bool save(matjson::Value& json) const override {
-        return true;
-    }
-
-    bool isDefaultValue() const override {
-        return true;
-    }
-
-    void reset() override {}
-
-    SettingNodeV3* createNode(float width) override;
-};
-
-class MyButtonSettingNodeV3 : public SettingNodeV3 {
-protected:
-    ButtonSprite* m_buttonSprite;
-    CCMenuItemSpriteExtra* m_button;
-
-    bool init(std::shared_ptr<MyButtonSettingV3> setting, float width) {
-        if (!SettingNodeV3::init(setting, width))
-            return false;
-        
-        m_buttonSprite = ButtonSprite::create("Click Me!", "goldFont.fnt", "GJ_button_01.png", .8f);
-        m_buttonSprite->setScale(.5f);
-        m_button = CCMenuItemSpriteExtra::create(
-            m_buttonSprite, this, menu_selector(MyButtonSettingNodeV3::onButton)
-        );
-        this->getButtonMenu()->addChildAtPosition(m_button, Anchor::Center);
-        this->getButtonMenu()->setContentWidth(60);
-        this->getButtonMenu()->updateLayout();
-
-        this->updateState(nullptr);
-        
-        return true;
-    }
-    
-    void updateState(CCNode* invoker) override {
-        SettingNodeV3::updateState(invoker);
-
-        auto shouldEnable = this->getSetting()->shouldEnable();
-        m_button->setEnabled(shouldEnable);
-        m_buttonSprite->setCascadeColorEnabled(true);
-        m_buttonSprite->setCascadeOpacityEnabled(true);
-        m_buttonSprite->setOpacity(shouldEnable ? 255 : 155);
-        m_buttonSprite->setColor(shouldEnable ? ccWHITE : ccGRAY);
-    }
-    void onButton(CCObject*) {
-        FLAlertLayer::create(
-            "Hello",
-            fmt::format(
-                "You clicked the button for setting <co>{}</c>!",
-                this->getSetting()->getDisplayName()
-            ),
-            "OK"
-        )->show();
-    }
-
-    void onCommit() override {}
-    void onResetToDefault() override {}
-
-public:
-    static MyButtonSettingNodeV3* create(std::shared_ptr<MyButtonSettingV3> setting, float width) {
-        auto ret = new MyButtonSettingNodeV3();
-        if (ret && ret->init(setting, width)) {
+    static PlaytestMenu* create() {
+        auto ret = new PlaytestMenu();
+        if (ret && ret->init(300.f, 240.f, "AI Layout Generator")) {
             ret->autorelease();
             return ret;
         }
@@ -91,25 +17,86 @@ public:
         return nullptr;
     }
 
-    bool hasUncommittedChanges() const override {
-        return false;
-    }
-    bool hasNonDefaultValue() const override {
-        return false;
+    bool setup() override {
+        this->setTitle("AI Layout Generator");
+
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+        auto label = CCLabelBMFont::create(
+            "Configure your AI-generated layout",
+            "bigFont.fnt"
+        );
+        label->setPosition({150.f, 200.f});
+        this->addChild(label);
+
+        // Example setting: difficulty slider
+        auto difficulty = Slider::create(
+            0.f, 1.f, 0.5f,
+            [](float value) {
+                log::info("AI difficulty set to {}", value);
+            }
+        );
+        difficulty->setPosition({150.f, 150.f});
+        this->addChild(difficulty);
+
+        // Example setting: length selector
+        auto lengthBtn = ButtonSprite::create("Length: Medium", "bigFont.fnt", "GJ_button_01.png");
+        auto length = CCMenuItemSpriteExtra::create(
+            lengthBtn, this, menu_selector(PlaytestMenu::onLength)
+        );
+        length->setPosition({150.f, 110.f});
+        this->m_buttonMenu->addChild(length);
+
+        // Generate button
+        auto generateBtn = ButtonSprite::create("Generate Layout", "bigFont.fnt", "GJ_button_01.png");
+        auto generate = CCMenuItemSpriteExtra::create(
+            generateBtn, this, menu_selector(PlaytestMenu::onGenerate)
+        );
+        generate->setPosition({150.f, 60.f});
+        this->m_buttonMenu->addChild(generate);
+
+        return true;
     }
 
-    std::shared_ptr<MyButtonSettingV3> getSetting() const {
-        return std::static_pointer_cast<MyButtonSettingV3>(SettingNodeV3::getSetting());
+    void onLength(CCObject*) {
+        log::info("Layout length changed");
+    }
+
+    void onGenerate(CCObject*) {
+        log::info("Running AI layout generator…");
+
+        // Call your AI generator here
+        // Example:
+        // auto layout = AI::generateLayout(difficultyValue, lengthValue);
+        // Editor::applyLayout(layout);
+
+        FLAlertLayer::create(
+            "AI Generator",
+            "Your layout has been generated and applied!",
+            "OK"
+        )->show();
     }
 };
 
-SettingNodeV3* MyButtonSettingV3::createNode(float width) {
-    return MyButtonSettingNodeV3::create(
-        std::static_pointer_cast<MyButtonSettingV3>(shared_from_this()),
-        width
-    );
-}
+class MyButtonSettingV3 : public SettingNode {
+public:
+    bool init() override {
+        if (!SettingNode::init()) return false;
 
-$execute {
-    (void)Mod::get()->registerCustomSettingType("my-button-type", &MyButtonSettingV3::parse);
-}
+        auto btnSprite = ButtonSprite::create("Playtest", "bigFont.fnt", "GJ_button_01.png");
+        auto btn = CCMenuItemSpriteExtra::create(
+            btnSprite, this, menu_selector(MyButtonSettingV3::onPlaytest)
+        );
+
+        auto menu = CCMenu::create();
+        menu->addChild(btn);
+        menu->setPosition({50.f, 20.f});
+        this->addChild(menu);
+
+        return true;
+    }
+
+    void onPlaytest(CCObject*) {
+        PlaytestMenu::create()->show();
+    }
+};
